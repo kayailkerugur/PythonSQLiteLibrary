e05da3924df09ec882968b4bda0721d987df64b2
ilkerugurkaya <kayailkerugur@gmail.com>
2023-01-21 16:15:25 +0300

    v0.2.2

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import sqlite3
 
 class Main:
     database = ""
-    def __init__(self,database):
+    table_name = ""
+    def __init__(self,database,table_name):
         self.database = database
+        self.table_name = table_name
 
     def connect(self) -> sqlite3.Connection:
         vt = sqlite3.connect(self.database)
@@ -16,6 +18,7 @@ class Main:
     
     def create_table(self,table_name):
         content_name = []
+        self.table_name = table_name
         while True:
             kullanici = input("Enter the content titles. (q to exit)")
             if kullanici == "q":
@@ -25,10 +28,17 @@ class Main:
         
         strings = list(filter(lambda x: isinstance(x, str), content_name))
         full_sentence = ",".join(strings)
+        self.content_name = full_sentence
         cursor = self.cursor()
         cursor.execute("CREATE TABLE " + table_name + " (" + full_sentence + ") ")
 
+    def add_table(self,content_name, content):
+        vt = self.connect()
+        cursor = self.cursor()
+        cursor.execute("INSERT INTO " + "bunungibi" + content_name + " VALUES " + content)
+        vt.commit()
 
-main = Main(database="databasee.sqlite")
 
-main.create_table(table_name="titles")
+main = Main(database="database.sqlite",table_name="bunungibi ")
+main.add_table(content_name="(deneme, amacli, kisiler)",content="('deneme', 'amacli', 'kisiler')")
+
