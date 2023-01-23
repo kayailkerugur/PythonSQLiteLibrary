8b7ad54895e2334ba2d6b42b756f814b158aa742
ilkerugurkaya <kayailkerugur@gmail.com>
2023-01-23 16:05:15 +0300

    v0.0.0.4

--- main.py
+++ main.py
@@ -43,5 +43,3 @@ class Main:
         return result
 
 
-main = Main(database="database.sqlite",table_name="bunungibi ")
-main.add_table(content_name="(deneme, amacli, kisiler)",content="('deneme', 'amacli', 'kisiler')")
